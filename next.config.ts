9de03cefaf014e3a1db46d18fa6fc709af4aea66
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-10-08 18:39:47 +0900

    #5 build(dashboard) : 빌드 설정 수정

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
+import path from 'path';
 import type { NextConfig } from 'next';
+import type { Configuration } from 'webpack';
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   reactStrictMode: true, // 리액트 스트릭트 모드 (개발 시 문제 탐지)
   compiler: {},
   images: {
     domains: ['example.com'], // 외부 이미지 도메인 허용
   },
-  experimental: {
-    turbo: true, // 터보 팩 사용
+  webpack(config: Configuration) {
+    (config.resolve!.alias as { [key: string]: string })['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 };
 
