231cdd28876935be3a023eb9c678ca80141ab9cd
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-10-08 12:11:38 +0900

    #0 feat(main): styled-components -> tailwind

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig = {
   reactStrictMode: true, // 리액트 스트릭트 모드 (개발 시 문제 탐지)
-  compiler: {
-    styledComponents: true, // styled-components SSR 지원
-  },
+  compiler: {},
   images: {
     domains: ['example.com'], // 외부 이미지 도메인 허용
   },
